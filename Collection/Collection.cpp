fde4b395512ce7d58829d63398c03c14ecc1e17a
Alex <sakalinski95@gmail.com>
2023-07-31 22:13:51 +0300

    Добавьте файлы проекта.

---
+++ Collection/Collection.cpp
@@ -0,0 +1,157 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <conio.h>
+#include <ctime>
+
+struct list_element
+{
+	int value;
+	struct list_element* next;
+};
+
+struct list_element* create_LIFO(int N)
+{
+	struct list_element* FIRST = NULL, * TMP = NULL;
+	for (int i = 0; i < N; i++)
+	{
+		TMP = FIRST;
+		FIRST = new list_element;
+		FIRST->next = TMP;
+		FIRST->value = rand();
+		printf("%d - %d\n", i + 1, FIRST->value);
+	}
+	return FIRST;
+}
+
+void print_list(struct list_element* list)
+{
+	if (list == NULL) {
+		puts("Empty list");
+		exit(0);
+	}
+	int i = 1;
+	while (list)
+	{
+		printf("%2.d ADDR: %p VALUE: %d\tADDR_NEXT: %p\n", i, list, list->value, list->next);
+		list = list->next;
+		i++;
+	}
+}
+
+list_element* AddStack(list_element* begin, int N) {
+	list_element* tmp = begin;
+	for (int i = 0; i < N; i++) {
+		tmp = begin;
+		begin = new list_element;
+		begin->next = tmp;
+		begin->value = rand();
+		printf_s("%2.d ADR : %p info : %d\t ADR_NEXT : %p\n", i + 1, begin, begin->value, begin->next);
+	}
+	return begin;
+}
+
+int FindMin(struct list_element* list)
+{
+	if (list == NULL) {
+		puts("Empty list");
+		exit(0);
+	}
+	int i = 1;
+	int min = list->value;
+	while (list)
+	{
+		if (min > list->value) {
+			min = list->value;
+		}
+		list = list->next;
+		i++;
+	}
+	printf("Minimum %d\n", min);
+	return min;
+}
+
+list_element* NewStack(list_element* list, list_element* newbegin, int min) {
+	if (list == NULL) {
+		puts("Empty list");
+		exit(0);
+	}
+	list_element* tmp = NULL;
+	int i = 0;
+	while (list) {
+		tmp = newbegin;
+		newbegin = new list_element;
+		newbegin->next = tmp;
+		newbegin->value = list->value;
+		if (list->value == min) {
+			list == NULL;
+			break;
+		}
+		list = list->next;
+		i++;
+	}
+	return newbegin;
+}
+
+void Delete(list_element* list) {
+	if (list == NULL) {
+		puts("Empty list");
+		exit(0);
+	}
+	list_element* tmp;
+	while (list) {
+		tmp = list;
+		list = list->next;
+		delete tmp;
+	}
+}
+
+void main() {
+	int s = 1;
+	int w, n;
+	struct list_element* FIRST = NULL;
+	list_element* newbegin = NULL;
+	srand(time(0));
+	for (s; s > 0 && s < 5; s = w) {
+		puts("1. Create list");
+		puts("2. Output created list");
+		puts("3. Add element");
+		puts("4. Task");
+		puts("5. Exit");
+		puts("Select an option");
+		scanf("%d", &w);
+		switch (w) {
+		case 1:
+			puts("Enter a quantity of elements");
+			scanf("%d", &n);
+			puts("List creation");
+			FIRST = create_LIFO(n);
+			break;
+		case 2:
+			puts("Created list");
+			print_list(FIRST);
+			break;
+		case 3:
+			int M;
+			puts("Enter a quantity of added elements");
+			scanf_s("%d", &M);
+			FIRST = AddStack(FIRST, M);
+			printf_s("begin ADR : %p", FIRST);
+			puts("\nList added\n");
+			print_list(FIRST);
+			break;
+		case 4:
+			int min = FindMin(FIRST);
+			newbegin = NewStack(FIRST, newbegin, min);
+			puts("New list");
+			print_list(newbegin);
+			_getch();
+			break;
+			break;
+		}
+	}
+	Delete(FIRST);
+}
+
+
+
---
+++ List/List.cpp
@@ -0,0 +1,211 @@
+﻿#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+struct Spis {
+	int info;
+	Spis* next;
+	Spis* prev;
+};
+
+void CreateSpis(Spis** begin, Spis** end) {
+	Spis* t = new Spis;
+	srand((unsigned)t);
+	t->info = rand() % 200 - 100;
+	if (*begin == NULL) {
+		t->next = NULL;
+		t->prev = NULL;
+		*begin = *end = t;
+	}
+	else {
+		t->next = NULL;
+		t->prev = *end;
+		(*end)->next = t;
+		*end = t;
+	}
+}
+
+void CreateSpis_pos(Spis** begin_pos, Spis** end_pos, int value) {
+	Spis* t = new Spis;
+	t->info = value;
+	if (*begin_pos == NULL) {
+		t->next = NULL;
+		t->prev = NULL;
+		*begin_pos = *end_pos = t;
+	}
+	else {
+		t->next = NULL;
+		t->prev = *end_pos;
+		(*end_pos)->next = t;
+		*end_pos = t;
+	}
+}
+
+void CreateSpis_neg(Spis** begin_neg, Spis** end_neg, int value) {
+	Spis* t = new Spis;
+	t->info = value;
+	if (*begin_neg == NULL) {
+		t->next = NULL;
+		t->prev = NULL;
+		*begin_neg = *end_neg = t;
+	}
+	else {
+		t->next = NULL;
+		t->prev = *end_neg;
+		(*end_neg)->next = t;
+		*end_neg = t;
+	}
+}
+
+void SpisViewBegin(Spis* t) {
+	int i = 1;
+	if (t == NULL) {
+		puts("List is empty");
+		return;
+	}
+	while (t) {
+		printf_s("%2.d ADR: %p info: %d\t ADR_NEXT: %p\n", i, t, t->info, t->next);
+		t = t->next;
+		i++;
+	}
+}
+void SpisViewEnd(Spis* t) {
+	int i = 1;
+	if (t == NULL) {
+		puts("List is empty");
+		return;
+	}
+	while (t) {
+		printf_s("%2.d ADR: %p info: %d\t ADR_NEXT: %p\n", i, t, t->info, t->prev);
+		t = t->prev;
+		i++;
+	}
+}
+
+void DeleteSpis(Spis** list) {
+	Spis* tmp;
+	while (*list != 0) {
+		tmp = *list;
+		*list = (*list)->next;
+		delete (tmp);
+	}
+}
+
+void main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int menu, menu2;
+	Spis* begin = NULL;
+	Spis* end = NULL;
+	Spis* begin_pos = NULL;
+	Spis* end_pos = NULL;
+	Spis* begin_neg = NULL;
+	Spis* end_neg = NULL;
+	Spis* t = NULL;
+
+	do {
+		puts("1 – Create list");
+		puts("2 – View list");
+		puts("3 – Add an element");
+		puts("4 – Create lists with positif elements and negatif elements");
+		puts("5 – Exit ");
+		puts("Choose an option");
+		scanf_s("%d", &menu);
+		switch (menu) {
+		case 1: {
+			DeleteSpis(&begin);
+			int N, i = 0;
+			puts("Create list");
+			puts("Enter a quantity of elements");
+			scanf_s("%d", &N);
+			for (int i = 0; i < N; i++) {
+				CreateSpis(&begin, &end);
+				printf_s("%d - %d\n", i + 1, end->info);
+			}
+			_getch();
+			break;
+		}
+		case 2: {
+			puts("View list");
+			puts("11 - From the beginning");
+			puts("22 - From the end");
+			puts("0 - Exit");
+			scanf_s("%d", &menu2);
+			switch (menu2) {
+			case 11: {
+				SpisViewBegin(begin);
+				_getch();
+				break;
+			}
+			case 22: {
+				SpisViewEnd(end);
+				_getch();
+				break;
+			}
+			case 0: {
+				exit(0);
+			}
+			default: {
+				puts("Incorrect choise");
+				_getch();
+				break;
+			}
+			}
+			break;
+		}
+		case 3: {
+			int M;
+			puts("Add an element");
+			puts("Enter a quantity of added elements");
+			scanf_s("%d", &M);
+			for (int i = 0; i < M; i++) {
+				CreateSpis(&begin, &end);
+				printf_s("%d - %d\n", i + 1, end->info);
+			}
+			_getch();
+			break;
+		}
+		case 4: {
+			DeleteSpis(&begin_pos);
+			DeleteSpis(&begin_neg);
+			t = begin;
+			if (t->next == t->prev) {
+				puts("List is empty");
+			}
+			else {
+				while (begin != 0) {
+					if (begin->info > 0) {
+						CreateSpis_pos(&begin_pos, &end_pos, begin->info);
+					}
+					else {
+						CreateSpis_neg(&begin_neg, &end_neg, begin->info);
+					}
+					begin = begin->next;
+				}
+			}
+			begin = t;
+			puts("List of positif elements");
+			SpisViewBegin(begin_pos);
+			puts("List of negatif elements");
+			SpisViewBegin(begin_neg);
+			_getch();
+			break;
+		}
+		case 5: {
+			exit(0);
+		}
+		default: {
+			puts("Incorrect value");
+			_getch();
+			break;
+		}
+		}
+	} while (menu != 5);
+	DeleteSpis(&begin);
+	DeleteSpis(&begin_pos);
+	DeleteSpis(&begin_neg);
+}
+
+
